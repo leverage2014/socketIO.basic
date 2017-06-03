abfd9df38ee3c2eece2524a923d00d8e6a52ce48
leverage2014 <myletter428@gmail.com>
2017-06-02 23:58:31 -0700

    added custom message event

--- server.js
+++ server.js
@@ -6,8 +6,21 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
+io.on('connection', function(socket){
 	console.log('User connected via socket.io!');
+
+	socket.on('message', function(message){
+		console.log('Message received: ' + message.text);
+
+		// io.emit : send to all 
+
+		// send to others except self
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to the chat application!'
+	});
 });
 
 http.listen(PORT, function(){
